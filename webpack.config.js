1770fef027d48937d659984dbfc05dc1b522acca
Sacha Morgese <sacha_morgese@waku-2.com>
2017-10-06 17:14:39 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'react']
+          }
+        }
+      }
+    ]
+  }
+}
+
